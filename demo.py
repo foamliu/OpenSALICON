32f11f892487d60958d648ba4c5d3ee788b860fa
Yang Liu <foamliu@yeah.net>
2018-06-19 11:55:26 +0800

    ValueError: operands could not be broadcast together with shapes (778,1000,3) (778,1000)

--- demo.py
+++ demo.py
@@ -5,9 +5,14 @@ import numpy as np
 if __name__ == '__main__':
     sal = Salicon()
     image = cv.imread('face.jpg')
+    height, width = image.shape[:2]
     map = sal.compute_saliency('face.jpg')
     # map contains saliency map in double format.
 
+    print('np.max(map): ' + str(np.max(map)))
+    print('np.min(map): ' + str(np.min(map)))
+
+    map = np.reshape(map, (height, width, 1))
     image = image * map
     image = image.astype(np.uint8)
 
